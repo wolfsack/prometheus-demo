9638c1a91f6a77a2eecb3009c0316cd1190426ba
wolfsack <wolfsack22@gmail.com>
2021-10-16 03:11:34 +0200

    comments/clean up

--- go-demo-app/main.go
+++ go-demo-app/main.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 )
 
+// very basic Metric data object
 type Metric struct {
 	Name    string
 	Value   string
@@ -17,11 +18,17 @@ type Metric struct {
 }
 
 func main() {
-
+	// port application is listening on
 	PORT := 8090
+
+	// in memory request counter
 	requestCounter := 0
 	mux := http.NewServeMux()
+
+	// explicit route so it doesnt fallback to "/" and raising the counter
 	mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(http.StatusInternalServerError) })
+
+	// simple landing page
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestCounter++
 		w.Write([]byte(`
@@ -35,26 +42,34 @@ func main() {
 			</html>	
 		`))
 	})
+
+	// endpoint for Prometheus to scrape
 	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		requestCounter++
+
+		// creating Metric
 		metric := Metric{
-			Name:    "request_counter",
-			Value:   strconv.Itoa(requestCounter),
-			Help:    "request_counter empty",
-			Type:    "counter",
-			Comment: "Number of requests received",
-			Labels: map[string]string{
+			Name:    "request_counter",             // can be freely choosen
+			Value:   strconv.Itoa(requestCounter),  // the value to expose (here the counter)
+			Help:    "request_counter empty",       // some additional info that Prometheus collects
+			Type:    "counter",                     // on of these: Counter, Gauge, Histogram, Summary, Untyped
+			Comment: "Number of requests received", // Prometheus ignores this
+			Labels: map[string]string{ // Possible labels, not implemented here
 				"": "",
 			},
 		}
-		w.Header().Add("Content-Type", "text/plain")
-		w.Write([]byte(fmt.Sprintf(`%s`, metricToString(&metric))))
+		w.Header().Add("Content-Type", "text/plain")                // set Content-Type Header
+		w.Write([]byte(fmt.Sprintf(`%s`, metricToString(&metric)))) // write metric into response after formatting it to a string
 	})
 
+	// start server
 	fmt.Printf("listening on 0.0.0.0:%d ...\n", PORT)
 	log.Fatal(http.ListenAndServe("0.0.0.0:8090", mux))
 }
 
+// basic formatting from Metric to string
+// for extended functionality use libraries
+// https://prometheus.io/docs/guides/go-application/
 func metricToString(m *Metric) string {
 	return fmt.Sprintf(
 		`# HELP %s
